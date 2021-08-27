0f624a8bb318b905fd5ad778da154b5bab67e0b2
devabhimanyu <dev.chauhan.singh.abhimanyu@gmail.com>
2021-08-27 11:08:27 +0530

    feat: 🎸 nodemon for dev env

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ msg: "hello there" });
+app.get('/', (req, res) => {
+  res.send({ msg: 'hello theres' });
 });
 
 const PORT = process.env.PORT || 5000;
